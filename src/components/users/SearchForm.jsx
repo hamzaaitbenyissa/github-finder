193bf71d8a257989bbf4b0cc848c68d65142c550
hamzaaitbenyissa <hamzaaitbenyissa@gmail.com>
2022-09-25 18:12:49 +0100

    aler components

--- src/components/users/SearchForm.jsx
+++ src/components/users/SearchForm.jsx
@@ -1,59 +1,67 @@
 import React, { useContext, useState } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import Alert from "../layout/Alert";
 
 function SearchForm() {
   const [text, setText] = useState("");
   const { fetchUsers, isLoaded, clearSearch } = useContext(GithubContext);
+  const [alert, setAlert] = useState(false);
 
   const handlechange = (e) => {
+    setAlert(false);
     setText(e.target.value);
   };
 
   const handlesubmit = (e) => {
     e.preventDefault();
-    if (text.trim() === "") alert("please write something");
-    else {
+    if (text.trim() === "") {
+      setAlert(true);
+    } else {
       fetchUsers(text);
       setText("");
     }
   };
 
   return (
-    <div className="mt-10 grid grid-cols-1  xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
-      <div>
-        <form onSubmit={handlesubmit}>
-          <div className="form-control">
-            <div className="relative">
-              <input
-                type="text"
-                placeholder="Search for"
-                className="input input-lg pr-40  input-bordered w-full"
-                value={text}
-                onChange={handlechange}
-              />
+    <>
+      {alert && <Alert />}
 
-              <button
-                type="submit"
-                className="btn-info absolute top-0 right-0 rounded-l-none btn btn-lg w-36"
-              >
-                GO
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-
-      {isLoaded && (
+      <div className="mt-10 grid grid-cols-1  xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
         <div>
-          <button
-            onClick={clearSearch}
-            className="btn btn-outline btn-warning btn-lg"
-          >
-            Clear
-          </button>
+          <form onSubmit={handlesubmit}>
+            <div className="form-control">
+              <div className="relative">
+                <input
+                  type="text"
+                  placeholder="Search for"
+                  className="input input-lg pr-40  input-bordered w-full"
+                  value={text}
+                  onChange={handlechange}
+                />
+
+                <button
+                  type="submit"
+                  className="btn-info absolute top-0 right-0 rounded-l-none btn btn-lg w-36"
+                >
+                  GO
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-      )}
-    </div>
+
+        {isLoaded && (
+          <div>
+            <button
+              onClick={clearSearch}
+              className="btn btn-outline btn-warning btn-lg"
+            >
+              Clear
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
